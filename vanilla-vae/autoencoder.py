c0a0ac8c1d1f65937b48b979534ef9261c3bf33a
Gabriel Vigliensoni <gabriel@vigliensoni.com>
2021-06-01 16:16:05 -0400

    Add: Implement VAE and reorganize.

--- vanilla-vae/autoencoder.py
+++ vanilla-vae/autoencoder.py
@@ -3,17 +3,21 @@ import pickle
 
 from tensorflow.keras import Model
 from tensorflow.keras.layers import Input, Conv2D, ReLU, BatchNormalization, \
-    Flatten, Dense, Reshape, Conv2DTranspose, Activation
+    Flatten, Dense, Reshape, Conv2DTranspose, Activation, Lambda
 from tensorflow.keras import backend as K
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.losses import MeanSquaredError
 import numpy as np
+import tensorflow as tf
+from tensorflow.python.keras.backend import log
 
 
-class Autoencoder:
+tf.compat.v1.disable_eager_execution()
+
+class VAE:
     """
-    Autoencoder represents a Deep Convolutional autoencoder architectures with 
-    mirrored encoder and decoder components.
+    VAE represents a Deep Convolutional variational autoencoder architecture 
+    with mirrored encoder and decoder components.
     """
 
     def __init__(self,
@@ -27,6 +31,7 @@ class Autoencoder:
         self.conv_kernels = conv_kernels # [3, 5, 3]
         self.conv_strides = conv_strides # [1, 2, 2]
         self.latent_space_dim = latent_space_dim # 2
+        self.reconstruction_loss_weight = 1000
 
         self.encoder = None
         self.decoder = None
@@ -45,8 +50,11 @@ class Autoencoder:
 
     def compile(self, learning_rate=0.0001):
         optimizer = Adam(learning_rate=learning_rate)
-        mse_loss = MeanSquaredError()
-        self.model.compile(optimizer=optimizer, loss=mse_loss)
+
+        self.model.compile(optimizer=optimizer, 
+                           loss=self._calculate_combined_loss,
+                           metrics=[self._calculate_reconstruction_loss,
+                                    self._calculate_kl_loss])
 
     def train(self, x_train, batch_size, num_epochs):
         self.model.fit(x_train, 
@@ -63,15 +71,41 @@ class Autoencoder:
     def load_weights(self, weights_path):
         self.model.load_weights(weights_path)
 
+    def reconstruct(self, images):
+        latent_representations = self.encoder.predict(images)
+        reconstructed_images = self.decoder.predict(latent_representations)
+        return reconstructed_images, latent_representations
+
     @classmethod
     def load(cls, save_folder="."):
         parameters_path = os.path.join(save_folder, "parameters.pkl")
         with open(parameters_path, "rb") as f:
             parameters = pickle.load(f)
-        autoencoder = Autoencoder(*parameters)
+        autoencoder = VAE(*parameters)
         weights_path = os.path.join(save_folder, "weights.h5")
         autoencoder.load_weights(weights_path)
         return autoencoder
+
+    def _calculate_combined_loss(self, y_target, y_predicted):
+        reconstruction_loss = self._calculate_reconstruction_loss(y_target, y_predicted)
+        kl_loss = self._calculate_kl_loss(y_target, y_predicted)
+        combined_loss = self.reconstruction_loss_weight * reconstruction_loss\
+                                                        + kl_loss
+        return combined_loss
+
+    def _calculate_reconstruction_loss(self, y_target, y_predicted):
+        error = y_target - y_predicted
+        reconstruction_loss = K.mean(K.square(error), axis=[1, 2, 3])
+        return reconstruction_loss
+
+    def _calculate_kl_loss(self, y_target, y_predicted):
+        kl_loss = -0.5 * K.sum(1 + self.log_variance - K.square(self.mu) - 
+                               K.exp(self.log_variance), axis=1)
+        return kl_loss
+
+
+
+
 
     def _create_folder_if_it_doesnt_exist(self, folder):
         if not os.path.exists(folder):
@@ -192,15 +226,29 @@ class Autoencoder:
         return x
 
     def _add_bottleneck(self, x):
-        """Flatten data and add bottleneck (Dense layer)."""
-        self._shape_before_bottleneck = K.int_shape(x)[1:] # ignore batch size
+        """Flatten data and add bottleneck with Gaussian sampling (Dense 
+        layer).
+        """
+        self._shape_before_bottleneck = K.int_shape(x)[1:] 
         x = Flatten()(x)
-        x = Dense(self.latent_space_dim, name="encoder_output")(x)
+        self.mu = Dense(self.latent_space_dim, name="mu")(x)
+        self.log_variance = Dense(self.latent_space_dim, 
+                                  name="log_variance")(x)
+
+        def sample_point_from_normal_distribution(args):
+            mu, log_variance = args
+            epsilon = K.random_normal(shape=K.shape(self.mu), mean=0., 
+                                                    stddev=1.)
+            sampled_point = mu + K.exp(log_variance / 2) * epsilon
+            return sampled_point
+
+        x = Lambda(sample_point_from_normal_distribution, \
+                   name="encoder_output")([self.mu, self.log_variance])
         return x
 
 
 if __name__ == "__main__":
-    autoencoder = Autoencoder(
+    autoencoder = VAE(
         input_shape=(28, 28, 1),
         conv_filters=(32, 64, 64, 64),
         conv_kernels=(3, 3, 3, 3),
@@ -210,10 +258,6 @@ if __name__ == "__main__":
     autoencoder.summary()
 
 
-
-# https://www.youtube.com/watch?v=TtyoFTyJuEY
-
-# Next : https://www.youtube.com/watch?v=SF1uAtU5-BU
 
 
 
